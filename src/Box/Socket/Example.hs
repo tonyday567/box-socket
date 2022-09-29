a3b0404fc36aa53c74e880bd693ffbb9574fc9fb
Tony Day <tonyday567@gmail.com>
2022-09-30 04:27:27 +1000

    Remove concurrency (#2)
    * box delay-fix fixes
    
    * concurrency removed
    
    * turned doctest testRun off
    
    * version bump
    
    * moved to hackage box

--- src/Box/Socket/Example.hs
+++ src/Box/Socket/Example.hs
@@ -15,7 +15,7 @@ module Box.Socket.Example where
 
 import Box
 import Box.Socket
-import Control.Concurrent.Classy.Async as C
+import Control.Concurrent.Async
 import Data.Bool
 import Data.Functor.Contravariant
 import Data.Text (Text, pack)
@@ -32,7 +32,7 @@ clientIO =
     (Box (contramap (pack . show) toStdout) fromStdin)
 
 q' :: IO a -> IO (Either () a)
-q' f = C.race (cancelQ fromStdin) f
+q' f = race (cancelQ fromStdin) f
 
 cancelQ :: Emitter IO Text -> IO ()
 cancelQ e = do
@@ -64,12 +64,12 @@ tClientIO xs =
 
 -- | main test run of client-server functionality
 -- the code starts a server in a thread, starts the client in the main thread, and cancels the server on completion.
--- >>> testRun
+-- > testRun
 -- [Left "receiver: received: echo:1",Right "echo:1",Left "receiver: received: echo:2",Right "echo:2",Left "receiver: received: echo:3",Right "echo:3",Left "receiver: received: close: 1000 \"received close signal: responder closed.\""]
 testRun :: IO [Either Text Text]
 testRun = do
-  a <- C.async (runServer defaultSocketConfig (responderApp (\x -> bool (Right $ "echo:" <> x) (Left "quit") (x == "q"))))
+  a <- async (runServer defaultSocketConfig (responderApp (\x -> bool (Right $ "echo:" <> x) (Left "quit") (x == "q"))))
   sleep 0.1
   r <- tClient (pack . show <$> [1 .. 3 :: Int])
-  C.cancel a
+  cancel a
   pure r
